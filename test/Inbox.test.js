9b264a5be8bfd2aaf8716a02dad3a76f11ff6c01
Mohammed Niyas <nzmniyascse@gmail.com>
2018-07-03 23:26:38 +0530

    used async to retrive accounts

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,17 +3,16 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-beforeEach(() => {
+let accounts;
+beforeEach(async () => {
     // Get list pf all accounts
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-            console.log(fetchedAccounts);
-        });
+    accounts = await web3.eth.getAccounts();
+        
     // Use one of these accounts to deploy the contracts
 });
 
 describe('Inbox', () => {
     it('Deploys a contract', () => {
-        
-    })
+        console.log(accounts);
+    });
 })
