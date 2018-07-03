4bf78c78950dc0505aa401a97c3b9693468950cd
Mohammed Niyas <nzmniyascse@gmail.com>
2018-07-03 23:45:05 +0530

    Deployed contract to test blockchain network using ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,17 +2,22 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
-let accounts;
+let accounts,
+    inbox;
 beforeEach(async () => {
     // Get list pf all accounts
     accounts = await web3.eth.getAccounts();
         
     // Use one of these accounts to deploy the contracts
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: ['Hi There!']})
+        .send({from: accounts[0], gas: '1000000'});
 });
 
 describe('Inbox', () => {
     it('Deploys a contract', () => {
-        console.log(accounts);
+        console.log(inbox);
     });
 })
