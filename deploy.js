26bdf917fa7c6b927cfe3208271ce2b5ef706e7d
Mohammed Niyas <nzmniyascse@gmail.com>
2018-07-05 01:32:28 +0530

    updated the deploy script

--- deploy.js
+++ deploy.js
@@ -3,8 +3,24 @@ const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
-    'absurd camera lake hurt sniff inquiry liquid bid lift earn security lottery',
-    'https://rinkeby.infura.io/cA9UUVGEirI1dErdkLNE',
+    "absurd camera lake hurt sniff inquiry liquid bid lift earn security lottery",
+    "https://rinkeby.infura.io/cA9UUVGEirI1dErdkLNE"
 );
 
-const web3 = new Web3(provider);
+const web3 = new Web3(provider); 
+
+const deploy = async () => {
+    console.log('default account', await web3.eth.getAccounts());
+        const accounts = await web3.eth.getAccounts();
+    
+        console.log('Attempting to deploy from account', accounts[0]);
+    
+        const inbox = await new web3.eth.Contract(JSON.parse(interface))
+            .deploy({ data: bytecode, arguments: ['Hi there!'] })
+            .send({ gas: '1000000', from: accounts[0] });
+        
+        console.log('Contract deployed to', inbox.options.address);    
+
+};
+
+deploy();
